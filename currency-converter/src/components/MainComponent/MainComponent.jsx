3a7717670eaed0dc61af6d349f70e72bb0316a7f
Borja <lopzdiazborja@gmail.com>
2023-09-20 17:57:43 +0200

    Añadimos parte de front y comenzamos a hacer funcionalidades

---
+++ currency-converter/src/components/MainComponent/MainComponent.jsx
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+
+function MainComponent() {
+  const [currentValue, setCurrentValue] = useState();
+  const [currentCurrency, setCurrentCurrency] = useState();
+  const [outputCurrency, setOutputCurrency] = useState();
+  const [resultVisible, setResultVisible] = useState(false);
+  const [codeSymbol, setCodeSymbol] = useState([]);
+
+  let objectEntries;
+
+  const fetchAPI = async () => {
+    try {
+      const resp = await fetch("https://api.exchangerate.host/symbols");
+      const data = await resp.json();
+      const objectValuesArray = Object.values(data.symbols);
+      setCodeSymbol(objectValuesArray);
+    } catch (error) {
+      console.log("Ha ocurrido un error. ", error.message);
+    }
+  };
+
+  const handleChange = async () => {
+    const resp = await fetch(
+      `https://api.exchangerate.host/convert?from=${currentCurrency}&to=${outputCurrency}`
+    );
+    const data = await resp.json();
+    console.log(data);
+  };
+
+  const handleCurrentChange = (e) => {
+    const inputValue = e.target.value;
+    // Utiliza una expresión regular para permitir solo números y el carácter "." para decimales
+    const numericValue = inputValue.replace(/[^0-9.]/g, "");
+
+    setCurrentValue(numericValue);
+    setResultVisible(false);
+  };
+
+  const handleDropdownMenuCurrent = (e) => {
+    setCurrentCurrency(e.target.value);
+    setResultVisible(false);
+  };
+
+  const handleDropdownMenuOutput = (e) => {
+    setOutputCurrency(e.target.value);
+    setResultVisible(false);
+  };
+
+  const buttonHandler = (e) => {
+    e.preventDefault();
+    handleChange();
+    setResultVisible(true);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <div className="Main">
+      <div className="container">
+        <div id="headerContainer">
+          <img src={`CurrencyIcon.svg`} alt="Imagen de la aplicación" />
+          <h2>CurrencyConverter</h2>
+        </div>
+        <div id="currencyInput">
+          <div id="currencyContainer">
+            <h2>Cantidad</h2>
+            <input
+              type="text"
+              value={currentValue}
+              style={{ textAlign: "center" }}
+              onChange={handleCurrentChange}
+              placeholder="Introduce cantidad"
+              required
+            />
+          </div>
+
+          <div id="selectCurrencyContainer">
+            <select
+              value={currentCurrency}
+              onChange={handleDropdownMenuCurrent}
+            >
+              <option value="">Selecciona una opción</option>
+              {codeSymbol.map((s, id) => {
+                return (
+                  <option value={`${s.code}`} key={id}>{`${s.code}`}</option>
+                );
+              })}
+            </select>
+            <select value={outputCurrency} onChange={handleDropdownMenuOutput}>
+              <option value="">Selecciona una opción</option>
+              {codeSymbol.map((s, id) => {
+                return (
+                  <option value={`${s.code}`} key={id}>{`${s.code}`}</option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
+
+        <div id="buttonCalculate">
+          <button onClick={buttonHandler}>Calcular</button>
+        </div>
+
+        <div className={`result ${resultVisible ? "visible" : ""}`}>
+          <h3>{`${currentValue} ${currentCurrency} = X ${outputCurrency}`}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainComponent;
